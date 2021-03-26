d9cbbf368bc2ef243b9151649b8031946dfefcb0
tmastrom <thomas.mastromonaco@gmail.com>
2021-03-26 14:43:59 -0700

    imporve accessibility and best practices score

--- pages/index.js
+++ pages/index.js
@@ -10,32 +10,34 @@ import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
+    <html lang="en">
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className={utilStyles.centerText} >Developer @ <a href="https://audette.io/">Audette</a></p>
+        <p className={utilStyles.centerText} >Developer @ <a href="https://audette.io/" alt="Audette homepage" target="_blank" rel="noreferrer">Audette</a></p>
       </section>
       <section className={`${utilStyles.centerText}`}>
         <ul>
           <li className={`${utilStyles.icon}`}>
-            <a href="https://github.com/tmastrom" target="_blank">
+            <a href="https://github.com/tmastrom" alt="github profile link" target="_blank" rel="noreferrer">
               <FontAwesomeIcon className={utilStyles.iconcolour} icon={faGithub} />
             </a>
           </li>
           <li className={utilStyles.icon}>
-            <a href="https://www.linkedin.com/in/tmastro/" target="_blank">
+            <a href="https://www.linkedin.com/in/tmastro/" alt="linkedin profile link" target="_blank" target="_blank" rel="noreferrer">
               <FontAwesomeIcon className={utilStyles.iconcolour} icon={faLinkedin}/>
             </a>
           </li>
           <li className={utilStyles.icon}>
-            <a href="thomas.mastromonaco@gmail.com" target="_blank">
+            <a href="thomas.mastromonaco@gmail.com" alt="email link" target="_blank" target="_blank" rel="noreferrer">
               <FontAwesomeIcon className={utilStyles.iconcolour} icon={faEnvelope}/>
             </a>
           </li>
         </ul>
       </section>
     </Layout>
+    </html>
   )
 }
