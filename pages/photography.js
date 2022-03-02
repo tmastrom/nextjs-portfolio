903c4ad9cccddd412f2906360ac1aa9206c219ee
Thomas Mastromonaco <tom@Thomass-MacBook-Pro.local>
2022-03-01 20:14:36 -0800

    set bg colour

--- pages/photography.js
+++ pages/photography.js
@@ -10,48 +10,36 @@ import utilStyles from '../styles/utils.module.css'
 export default function Photography({data}) {
   const [photos, setPhotos] = useState(data);
   return (
-    <ChakraProvider>
+    
+      <Layout>
         <Head>
           <title>{siteTitle}</title>
         </Head>
-
-        <Box overflow="hidden" bg="purple.100" minH="100vh">
-          <Container>
-            <Text
-              color="pink.800"
-              fontWeight="semibold"
-              mb="1rem"
-              textAlign="center"
-              textDecoration="underline"
-              fontSize={["4xl", "4xl", "5xl", "5xl"]}
-            >
-              NextJS Image Gallery
-            </Text>
-          </Container>
-
-          <Wrap px="1rem" spacing={4} justify="center">
-            
-              
-            <WrapItem
-              key={photos.id}
-              boxShadow="base"
-              rounded="20px"
-              overflow="hidden"
-              bg="white"
-              lineHeight="0"
-              _hover={{ boxShadow: "dark-lg" }}
-            >
-              <Image 
-                src={photos.src.original} 
-                width="500" 
-                height="500" 
-                alt={photos.url}/>
-            </WrapItem>
-              
-            
-          </Wrap>
-        </Box>
-    </ChakraProvider>
+        <ChakraProvider>
+          <Box overflow="hidden" bg="#fefae0" minH="100vh">
+            <Container>
+              <Wrap px="1rem" spacing={4} justify="center">
+                  <WrapItem
+                    key={photos.id}
+                    boxShadow="base"
+                    rounded="20px"
+                    overflow="hidden"
+                    bg="white"
+                    lineHeight="0"
+                    _hover={{ boxShadow: "dark-lg" }}
+                  >
+                    <Image 
+                      src={photos.src.original} 
+                      width="500" 
+                      height="500" 
+                      alt={photos.url}/>
+                  </WrapItem>
+                </Wrap>
+              </Container>
+          </Box>
+        </ChakraProvider>
+      </Layout>
+        
   )
 }
 
