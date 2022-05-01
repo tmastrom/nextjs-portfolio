acc33adfdf1ba1150a84f9338e5a80940d474541
Thomas Mastromonaco <tmastro@Thomass-MacBook-Air.local>
2022-05-01 14:16:23 -0500

    remove pexels api

--- pages/photography.js
+++ pages/photography.js
@@ -1,53 +1,31 @@
 import React, { useState } from "react";
-import { ChakraProvider, Box, Container, Text, Wrap, WrapItem } from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from "next/image";
 
-import {getCuratedPhotos} from "../lib/api"
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+
+const imageList = [
+  <Image src="/images/gallery/bangkok.jpg" alt="me" width="64" height="64" />,
+  <Image src="/images/gallery/cotopaxi.jpg" alt="me" width="64" height="64" />,
+  <Image src="/images/gallery/erawan.jpg" alt="me" width="64" height="64" />,
+  <Image src="/images/gallery/malpaso.jpeg" alt="me" width="64" height="64" />,
+  <Image src="/images/gallery/tofino.jpg" alt="me" width="64" height="64" />,
+  <Image src="/images/gallery/whistler.jpg" alt="me" width="64" height="64" />
+];
 
 export default function Photography({data}) {
-  const [photos, setPhotos] = useState(data);
+
   return (
     
       <Layout>
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <ChakraProvider>
-          <Box overflow="hidden" bg="#fefae0" minH="100vh">
-            <Container>
-              <Wrap px="1rem" spacing={4} justify="center">
-                  <WrapItem
-                    key={photos.id}
-                    boxShadow="base"
-                    rounded="20px"
-                    overflow="hidden"
-                    bg="white"
-                    lineHeight="0"
-                    _hover={{ boxShadow: "dark-lg" }}
-                  >
-                    <Image 
-                      src={photos.src.original} 
-                      width="500" 
-                      height="500" 
-                      alt={photos.url}/>
-                  </WrapItem>
-                </Wrap>
-              </Container>
-          </Box>
-        </ChakraProvider>
+        <ul>
+          {imageList}
+        </ul>
+
       </Layout>
         
   )
-}
-
-export async function getServerSideProps() {
-  const data = await getCuratedPhotos();
-  return {
-    props: {
-      data,
-    },
-  };
 }
